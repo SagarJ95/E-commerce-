7d448187f902c7ddf541ae76f268f23bb6e98bca
SagarJ95 <155131844+SagarJ95@users.noreply.github.com>
2024-03-27 18:24:46 +0530

    updateView

--- app.js
+++ app.js
@@ -14,6 +14,36 @@ app.get("/", (req, res) => {
   res.render("login");
 });
 
+//about page
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+//service page
+app.get("/services", (req, res) => {
+  res.render("services");
+});
+
+// blog page
+app.get("/blog", (req, res) => {
+  res.render("blog");
+});
+
+//contact page
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+//cart page
+app.get("/cart", (req, res) => {
+  res.render("cart");
+});
+
+//shop page
+app.get("/shop", (req, res) => {
+  res.render("shop");
+});
+
 app.listen(PORT, () => {
   console.log("connnect");
 });
